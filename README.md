4d7168a5090c3f642ee52917fe29237bee0f0644
Herbie <843834089@qq.com>
2018-05-22 11:21:10 +0800

    feat(优化): 优化响应头&&配置npm包管理
    优化响应头配置&&配置npm包管理工具&&启动自动打开浏览器&&命令行输出颜色字体

---
+++ README.md
@@ -0,0 +1,4 @@
+
+##启动方式
+cmd下开启服务：
+$ node server.js || npm run start  
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var fs = require('fs'); //引入fs模块
 var url = require('url');//引入url模块
 var path = require('path');//引入path模块
 
+var cp = require('child_process');  // 可自动打开浏览器模块
+var chalk = require('chalk');   //命令行输出字体颜色
 
 // req : 从浏览器带来的请求信息
 // res : 从服务器返回给浏览器的信息
@@ -11,7 +13,6 @@ var server = http.createServer(function(req,res){
     var pathname = url.parse(req.url).pathname;;
      //客户端输入的url，例如如果输入localhost:8888/index.html，那么这里的url == /index.html 
      //url.parse()方法将一个URL字符串转换成对象并返回，通过pathname来访问此url的地址。
-
     // var realPath = path.join('F:/nodejs/nodetest',pathname);
     var realPath = path.join('D:/study.project/Node/nodeServe',pathname);
     //完整的url路径
@@ -34,17 +35,44 @@ var server = http.createServer(function(req,res){
             res.end();
         }else{
             //成功读取文件
-            res.writeHead(200,{
-                // 'content-type':'text/html;charset=utf-8'
-            });
+            // res.writeHead(200,{
+            //     // 'content-type':'text/html;charset=utf-8'
+            // });
+            rsFn(res, pathname)
             res.write(data);
             res.end();
         }
     })
 });
 server.listen(PORT); //监听端口
-console.log('服务成功开启')
+// console.log('服务成功开启')
+console.log(chalk.green('服务成功开启'))
+console.log(chalk.green(`http://127.0.0.1:${PORT} || http://localhost:${PORT}`))
+//cp.exec(`start http://127.0.0.1:${PORT}`);  // 自动打开默认浏览器
+//cp.exec(`start http://127.0.0.1:${PORT}/index.html`);  // 自动打开默认浏览器
 
-//cmd下开启服务：
 
-//$ node server.js
+var rsFn = function(res, pathname) {
+        switch(path.extname(pathname)){ // 不同文件返回不同类型
+            case ".html":
+                res.writeHead(200, {"Content-Type": "text/html"});
+                break;
+            case ".js":
+                res.writeHead(200, {"Content-Type": "text/javascript"});
+                break;
+            case ".css":
+                res.writeHead(200, {"Content-Type": "text/css"});
+                break;
+            case ".gif":
+                res.writeHead(200, {"Content-Type": "image/gif"});
+                break;
+            case ".jpg":
+                res.writeHead(200, {"Content-Type": "image/jpeg"});
+                break;
+            case ".png":
+                res.writeHead(200, {"Content-Type": "image/png"});
+                break;
+            default:
+                res.writeHead(200, {"Content-Type": "application/octet-stream"});
+        }        
+}
